9a771c1647a16e0fbec443642158d7ed33bc2b4a
FranckDK <franck@piwowarczyk.com>
2015-12-04 16:25:52 +0100

    04/12/2015-16h25

--- action.php
+++
@@ -1,9 +0,0 @@
-<!--ACTION.PHP :)-->
-<?php
-@$type=addslashes($_GET['type']);
-@$mod=addslashes($_GET['mod']);
-@$fonc=addslashes($_GET['fonc']);
-if ($type =='CONFIG' && $mod!=""){include('10-ADMIN/01-CONFIG/'.$mod.'/01-FONCTIONS.php');}
-if ($type =='APPLIS' && $mod!=""){include('10-ADMIN/02-APPLIS/'.$mod.'/01-FONCTIONS.php');}
-if ($fonc !=''){$fonc();}
-?>
